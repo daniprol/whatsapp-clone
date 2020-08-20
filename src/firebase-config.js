26180239645ecdb887bedb18b0bd190a3ad42110
Dani Prol <daniprol@hotmail.com>
2020-08-20 17:59:48 +0200

    Initial structure

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXkkc0tG-cdIQBcCoXbKmTiV-QakTXvNo",
+  authDomain: "whatsapp-clone-cdce0.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-cdce0.firebaseio.com",
+  projectId: "whatsapp-clone-cdce0",
+  storageBucket: "whatsapp-clone-cdce0.appspot.com",
+  messagingSenderId: "861840966732",
+  appId: "1:861840966732:web:7eb24a232e622a3c51e0e9",
+  measurementId: "G-ZXR0VEMHF8",
+};
