fe15966591b7ea2592a8b283a9663ed5c238e7ed
Dani Prol <daniprol@hotmail.com>
2020-08-20 21:52:36 +0200

    SidebarChat design finished

---
+++ src/SidebarChat.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import "./SidebarChat.css";
+import { Avatar } from "@material-ui/core";
+
+function SidebarChat() {
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    const randomString = Math.random()
+      .toString(36)
+      .replace(/[^a-z]+/g, "");
+    setSeed(randomString);
+  }, []);
+  //   console.log(randomString);
+  return (
+    <div className="sidebarChat">
+      <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>Last message...</p>
+      </div>
+    </div>
+  );
+}
+
+export default SidebarChat;
