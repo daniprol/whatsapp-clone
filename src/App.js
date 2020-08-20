62ed76baa623b20f507b7515b1dec33f70dc8c77
Dani Prol <daniprol@hotmail.com>
2020-08-20 21:52:15 +0200

    Sidebar designed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Chat */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
