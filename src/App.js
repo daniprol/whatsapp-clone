63653ce0be8ad202c50f3d9cd63945d0b65693c5
Dani Prol <daniprol@hotmail.com>
2020-08-22 21:30:52 +0200

    Login implemented with useContext

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Sidebar from "./Sidebar";
 import Login from "./Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Chat";
+import { useStateValue } from "./StateProvider";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
+  // const [state, dispatch] = useStateValue();
+  // We can also do destructuring directly:
+  const [{ user }, dispatch] = useStateValue();
 
   const indexPage = (
     <div className="app__body">
--- src/Login.js
+++ src/Login.js
@@ -3,12 +3,22 @@ import "./Login.css";
 import { Button } from "@material-ui/core";
 import Logo from "./logo.svg";
 import { auth, provider } from "./firebase-config";
+import { useStateValue } from "./StateProvider";
+import { actionTypes } from "./reducer";
 
 function Login() {
+  const [state, dispatch] = useStateValue();
+  // We can also do destructuring
+
   const signIn = () => {
     auth
       .signInWithPopup(provider)
-      .then((result) => console.log(result))
+      .then((result) =>
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        })
+      )
       .catch((error) => alert(error.message));
   };
   return (
