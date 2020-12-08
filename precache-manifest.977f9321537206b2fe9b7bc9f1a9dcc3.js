ebb59039d5815a9e45748cb9ba475a449c3c0488
Dani Prol <daniprol@hotmail.com>
2020-12-08 23:50:25 +0100

    Updates

---
+++ precache-manifest.977f9321537206b2fe9b7bc9f1a9dcc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76de196262daaeb80698c1c394f9e4d6",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "837161ae51f03fd216f3",
+    "url": "/whatsapp-clone/static/css/main.0831ab73.chunk.css"
+  },
+  {
+    "revision": "048b2c85d47c010b2b1f",
+    "url": "/whatsapp-clone/static/js/2.39e18061.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp-clone/static/js/2.39e18061.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837161ae51f03fd216f3",
+    "url": "/whatsapp-clone/static/js/main.47bfed63.chunk.js"
+  },
+  {
+    "revision": "8d0a8cdab6927a1f4f08",
+    "url": "/whatsapp-clone/static/js/runtime-main.a04c35f0.js"
+  },
+  {
+    "revision": "ba90dc77fb82574fa4e224e804df95aa",
+    "url": "/whatsapp-clone/static/media/logo.ba90dc77.svg"
+  }
+]);
